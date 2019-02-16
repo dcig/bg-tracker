df38df94b41aefc1aa4b45f174d3ed7bf1b5c6a2
Dan Cigler <dancigler@gmail.com>
2019-02-16 13:51:13 -0600

    WIP

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        @user.save
+        redirect_to @user
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
+
+    private
+    def user_params
+        params.require(:users).permit(:first_name, :last_name)
+    end
+
+
+end
