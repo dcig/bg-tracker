b1b5bae656fa82d3b2fcd37c00a2172f6226bdd4
Dan Cigler <dancigler@gmail.com>
2019-05-28 19:22:50 -0500

    Mimicked Bolus for Basal Doses

---
+++ app/controllers/basal_doses_controller.rb
@@ -0,0 +1,29 @@
+class BasalDosesController < ApplicationController
+
+    def new
+         @basal_dose = BasalDose.new
+    end
+
+    def create
+        @basal_dose = BasalDose.create((basal_dose_params).merge(user_id: current_user.id))
+
+        if @basal_dose.save
+            redirect_to '/welcome/index'
+        else
+            render 'new'
+        end
+    
+    end
+
+    #def update
+        #@basal_dose = current_basal_dose
+        #@basal_dose.update!(basal_dose_params)
+         
+        #redirect_to @basal_dose
+    #end 
+
+    private
+    def basal_dose_params
+        params.require(:basal_dose).permit(:amount, :time_stamp)
+    end
+end
