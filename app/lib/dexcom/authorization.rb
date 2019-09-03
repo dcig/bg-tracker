5dfcba2de393a34db7f2fe8d36842efebcc566fd
Dan Cigler <dancigler@gmail.com>
2019-09-03 07:53:22 -0500

    Updated RestClient dependencies. Continued GET request for Dex

--- app/lib/dexcom/authorization.rb
+++ app/lib/dexcom/authorization.rb
@@ -57,20 +57,33 @@ module Dexcom
                     :code => 'authcode5',
                     :grant_type => 'authorization_code',
                     :redirect_uri => ENV['DEXCOM_REDIRECT']}
+
+                    #=> <RestClient::Response 200 "{\"access_to...">
+             #{
+                #"access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IncyYUVpQmRlMXBfNnNjZmMzXzFpeHFvaDVqTSIsImtpZCI6IncyYUVpQmRlMXBfNnNjZmMzXzFpeHFvaDVqTSJ9.eyJpc3MiOiJodHRwczovL3VhbTEuZGV4Y29tLmNvbS9pZGVudGl0eSIsImF1ZCI6Imh0dHBzOi8vdWFtMS5kZXhjb20uY29tL2lkZW50aXR5L3Jlc291cmNlcyIsImV4cCI6MTYzODcyOTE2MCwibmJmIjoxNTQ0MTIxMTYwLCJjbGllbnRfaWQiOiJLYlNMcW5xcGJHRVdmR0xua1lwaWxPa2x1ak9FUVBTWSIsInNjb3BlIjpbImVndiIsImNhbGlicmF0aW9uIiwiZGV2aWNlIiwic3RhdGlzdGljcyIsImV2ZW50Iiwib2ZmbGluZV9hY2Nlc3MiXSwic3ViIjoiOTM3YzIwYzctMjk4ZS00Mzc4LTg4YmEtMzllYThkNTMyMDA4IiwiYXV0aF90aW1lIjoxNTQ0MTIxMTI0LCJpZHAiOiJpZHNydiIsImp0aSI6ImJhYThjZGRhMGNlOTlhNzgxNTgyNTYxMGZiY2YxZGU5IiwiYW1yIjpbInBhc3N3b3JkIl19.QaqegOskvhjlkroPI72GDwBROT_ir3S05j0bjqFvWuhWc5BtT1YXoCH9OI-tTW9Pp-I-HMEh9ytu0mRTkA3rOHKj4SI-8XC_I8VBMwdS5Cm9mm-1HxJlcyRkUgmPVIxSXlGoFLI0xrPAkFYyVOReYs4yU5QzqXjV9agQ7FVUCONK6L-h3m03YeCh0hzKhw1VHzpvBJUSjY6j-8-ktn0Y_XoHWBET8Ep1GbdARNXpCxNdVyH_bK-Qqf2qVqT6zBml3idxcQjG-zPsUO5xGJEF3kekD4PzQev4JxA1O-Uht-dZqgGtcSAXuEp1fr5OIKtVAg0vqT_o37t_DoXjMlecWg",
+                #"refresh_token": "refreshtoken5",
+                #"expires_in": 600,
+                #"token_type": "Bearer"
+            #}
+
             #  retrive access_token, refresh_token, expires_in
             #  make an api request to the token endpoint. 
             #  It needs:
             #  params -- see the "params" we send in postman
             #  headers -- see the "headers" we send in postman
-            response = RestClient.get "https://sandbox-api.dexcom.com/v2/oauth2/token", {accept: :json}
-            respone.body
-            puts response.read_body
 
-
-
-            #  create a new DexcomAccessToken with that info & related to this user
+            # create a new DexcomAccessToken with that info & related to this user
 
             # return that token
+
+            #Normally, when you use RestClient.get or the lower level RestClient::Request.execute method: :get to retrieve data, 
+            #the entire response is buffered in memory and returned as the response to the call.
+            response = RestClient.get "https://sandbox-api.dexcom.com/v2/oauth2/token", {accept: :json}
+            json = JSON.parse(response)
+            json['code'] = response.code
+
+            respone.body
+            puts response.read_body
         end
 
         def refresh_token!(refresh_token)
---
+++ app/lib/dexcom/evgs.rb
@@ -0,0 +1,22 @@
+module Dexcom
+    class Evgs
+
+        attr_reader :user
+        def initialize(user)
+            @user = user
+        end
+        
+        def dexcom_bg!
+            require 'rest-client'
+            require 'json'
+            #Normally, when you use RestClient.get or the lower level RestClient::Request.execute method: :get to retrieve data, 
+            #the entire response is buffered in memory and returned as the response to the call.
+            response = RestClient.get "https://sandbox-api.dexcom.com/v2/oauth2/token", {accept: :json}
+            json = JSON.parse(response)
+            json['code'] = response.code
+
+            respone.body
+            puts response.read_body
+
+    end
+end
