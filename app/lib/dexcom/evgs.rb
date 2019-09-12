19a0f85f966c08009791c78f949c74ccef232176
Dan Cigler <dancigler@gmail.com>
2019-09-12 07:13:10 -0500

    evgs endpoint GET call

--- app/lib/dexcom/evgs.rb
+++ app/lib/dexcom/evgs.rb
@@ -23,9 +23,8 @@ module Dexcom
             evgs_payload = JSON.parse(response.body, symbolize_names: true)
 
             GlucoseReading.create!(
-                access_token: oauth_payload[:access_token],
-                expires_in: oauth_payload[:expires_in],
-                refresh_token: oauth_payload[:refresh_token],
+                bg_value: evgs_payload[:value],
+                time_stamp: evgs_payload[:displayTime],
                 user: @user
             )
            # {
@@ -40,26 +39,6 @@ module Dexcom
            #             "trend": "flat",
            #             "trendRate": 0.4,
            #             "realtimeValue": 183,
-           #             "smoothedValue": null
-           #         },
-           #         {
-           #             "systemTime": "2017-06-16T17:05:00",
-           #             "displayTime": "2017-06-16T09:05:00",
-           #             "value": 181,
-           #             "status": null,
-           #             "trend": "flat",
-           #             "trendRate": 0.5,
-           #             "realtimeValue": 181,
-           #             "smoothedValue": null
-           #         },
-           #         {
-           #             "systemTime": "2017-06-16T17:00:00",
-           #             "displayTime": "2017-06-16T09:00:00",
-           #             "value": 179,
-           #             "status": null,
-           #             "trend": "flat",
-           #             "trendRate": 0.6,
-           #             "realtimeValue": 179,
            #             "smoothedValue": null
            #         }
            #     ]
